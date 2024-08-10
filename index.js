50971d9eff7f5fccc369aeaa92829b8634d27402
Yashita <yashitabr04@gmail.com>
2024-08-10 22:38:19 +0530

    First

--- index.js
+++ index.js
@@ -10,18 +10,19 @@ app.use(bodyParser.urlencoded({
     extended:true
 }))
 
-mongoose.connect('mongodb://localhost:27017/Database')
+mongoose.connect('mongodb://localhost:27017/test')
 var db=mongoose.connection
 db.on('error',()=> console.log("Error in Connecting to Database"))
 db.once('open',()=> console.log("Connected to Database"))
 
-app.post("/sign_up",(req,res) => {
+app.post("/doctor_up",(req,res) => {
     var name= req.body.name
     var age=req.body.age
     var email=req.body.email
     var phno=req.body.phno
     var gender=req.body.gender
-    var password=req.body.password
+    var qualification=req.body.qualification
+    var specialisation=req.body.specialisation
 
     var data={
         "name":name,
@@ -29,9 +30,10 @@ app.post("/sign_up",(req,res) => {
         "email":email,
         "phno":phno,
         "gender":gender,
-        "password":password
+        "qualification":qualification,
+        "specialisation":specialisation
     }
-    db.collection('users').insertOne(data,(err,collection) => {
+    db.collection('doctor').insertOne(data,(err,collection) => {
         if(err){
             throw err;
         }
@@ -40,11 +42,87 @@ app.post("/sign_up",(req,res) => {
     return res.redirect('signup_successful.html')
 })
 
+
+app.post("/nurse_up",(req,res) => {
+    var name= req.body.name
+    var age=req.body.age
+    var email=req.body.email
+    var phno=req.body.phno
+    var gender=req.body.gender
+    var qualification=req.body.qualification
+    var department=req.body.department
+
+    var data={
+        "name":name,
+        "age":age,
+        "email":email,
+        "phno":phno,
+        "gender":gender,
+        "qualification":qualification,
+        "department":department
+    }
+    db.collection('nurse').insertOne(data,(err,collection) => {
+        if(err){
+            throw err;
+        }
+        console.log("Record Inserted Succesfully")
+    })
+    return res.redirect('signup_successful.html')
+})
+
+app.post("/patient_up",(req,res) => {
+    var name= req.body.name
+    var age=req.body.age
+    var email=req.body.email
+    var phno=req.body.phno
+    var gender=req.body.gender
+    var purpose=req.body.purpose
+
+    var data={
+        "name":name,
+        "age":age,
+        "email":email,
+        "phno":phno,
+        "gender":gender,
+        "purpose":purpose
+    }
+    db.collection('patient').insertOne(data,(err,collection) => {
+        if(err){
+            throw err;
+        }
+        console.log("Record Inserted Succesfully")
+    })
+    return res.redirect('signup_successful.html')
+})
+
+app.post("/visitor_up",(req,res) => {
+    var name= req.body.name
+    var age=req.body.age
+    var email=req.body.email
+    var phno=req.body.phno
+    var gender=req.body.gender
+    var purpose=req.body.purpose
+    var data={
+        "name":name,
+        "age":age,
+        "email":email,
+        "phno":phno,
+        "gender":gender,
+        "purpose":purpose
+    }
+    db.collection('visitor').insertOne(data,(err,collection) => {
+        if(err){
+            throw err;
+        }
+            console.log("Record Inserted Succesfully")
+    })
+    return res.redirect('signup_successful.html')
+})
 app.get("/",(req,res) => {
     res.set({
         "Allow-acces-Allow-Origin":'*'
     })
-    return res.redirect('index.html')
+    return res.redirect('indexss.html')
 }).listen(3000);
 
 console.log("Listening on port 3000")
